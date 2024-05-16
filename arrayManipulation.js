f105603afa484e414ddeadbd8b5dc386e7f3016d
Klenam <cedeegbe@gmail.com>
2024-05-16 11:38:39 +0000

    formatArrayString

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,6 +11,38 @@ function processArray(arr) {
   return newArray;
 }
 
-const arr = [5, 6, 1, 6, 12, 14];
-const newArray = processArray(arr);
+// const arr = [5, 6, 1, 6, 12, 14]; this could be an alternative but I decided to do it this way.
+const newArray = processArray([8, 7, 6, 13, 16, 19, 20, 17, 23]);
 console.log(newArray);
+
+function formatArrayStrings(arrStr, arrNum) {
+  const newStrings = [];
+
+  for (let i = 0; i < arrStr.length; i++) {
+    let string = arrStr[i];
+    let number = arrNum[i];
+
+    if (number % 2 === 0) {
+      newStrings.push(string.toUpperCase());
+    } else {
+      newStrings.push(string.toLowerCase());
+    }
+  }
+
+  return newStrings;
+}
+
+const message = [
+  "this",
+  "is",
+  " a",
+  " new ",
+  "string",
+  "formatted",
+  "using",
+  "a",
+  "function",
+];
+
+const newMessage = formatArrayStrings(message, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(newMessage);
